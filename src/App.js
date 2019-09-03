91cdf9f7d89e2e9f6f5285b5053582efe974b8c6
Michał Boruch <miboruch@gmail.com>
2019-09-03 10:13:28 +0200

    Hamburger component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Header/Header';
+import Hamburger from './components/Hamburger/Hamburger';
 
 const App = () => {
-  return <Header></Header>;
+  const [isHamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setHamburgerOpen(!isHamburgerOpen);
+  };
+
+  return (
+    <div className='App'>
+      <Hamburger isOpen={isHamburgerOpen} toggleMenu={toggleMenu}></Hamburger>
+      <Header></Header>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Hamburger/Hamburger.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHamburger = styled.button`
+  cursor: pointer;
+  width: 36px;
+  height: 36px;
+  background: transparent;
+  border: none;
+  z-index: 10;
+  margin: 0.5rem;
+  position: fixed;
+  top: 0;
+  right: 0;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const InnerHamburger = styled.div`
+  width: 26px;
+  height: 2px;
+  position: relative;
+  background: ${({ isOpen }) => (isOpen ? 'transparent' : 'black')}
+
+  ::before,
+  ::after {
+    content: '';
+    position: absolute;
+    width: 26px;
+    height: 2px;
+    background: ${({ isOpen }) => (isOpen ? 'white' : 'black')}
+    left: 0;
+    transition: all .3s ease;
+  }
+
+  ::before {
+    transform: rotate(${({ isOpen }) => (isOpen ? '40deg' : '0')});
+    top: ${({ isOpen }) => (isOpen ? '0' : '6px')}
+  }
+
+  ::after {
+    transform: rotate(${({ isOpen }) => (isOpen ? '-40deg' : '0')});
+    top: ${({ isOpen }) => (isOpen ? '0' : '-6px')}
+  }
+`;
+
+const Hamburger = ({ isOpen, ...props }) => {
+  return (
+    <StyledHamburger onClick={props.toggleMenu}>
+      <InnerHamburger></InnerHamburger>
+    </StyledHamburger>
+  );
+};
+
+export default Hamburger;
